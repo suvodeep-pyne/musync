a93c0195e6a35a7edf3f800e7e42f3269cfd0ea9
pranamibhatt <pranami.nits@gmail.com>
2013-11-22 18:55:08 -0500

    Client socket

--- MusyncProject/src/com/ecen602/musync/CommunicationSlave.java
+++ MusyncProject/src/com/ecen602/musync/CommunicationSlave.java
@@ -1,11 +1,53 @@
 package com.ecen602.musync;
 
-public class CommunicationSlave implements Runnable {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.Reader;
 
+import android.util.Log;
+
+
+public class CommunicationSlave implements Runnable {
+	
+	Socket socket;
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		try {
+			socket = new Socket("192.168.1.3", 5000);
+			communicate();
+
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
+		// communicate with server
+	public void communicate() {
+	try {
+        ObjectInputStream objectInput = 
+        		new ObjectInputStream(socket.getInputStream());
+        Packet packet;
+		try {
+			packet = (Packet) objectInput.readObject();
+			System.out.println("Client received" + packet);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+    } 
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+		}	
+
+		
+	//}
 
 }
--- MusyncProject/src/com/ecen602/musync/Constants.java
+++ MusyncProject/src/com/ecen602/musync/Constants.java
@@ -2,4 +2,6 @@ package com.ecen602.musync;
 
 public class Constants {
 	public static final int PORT = 5000;
+	public static final String ServerIP = "10.0.2.1";
+	// hard coded serverip, to be changed later
 }
