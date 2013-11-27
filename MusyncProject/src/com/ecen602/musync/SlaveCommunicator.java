ae5e69cbb7cedebe150874147da5c8efc500f024
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2013-11-26 22:48:42 -0600

    SlaveCommunicator

--- MusyncProject/src/com/ecen602/musync/SlaveCommunicator.java
+++ MusyncProject/src/com/ecen602/musync/SlaveCommunicator.java
@@ -12,30 +12,18 @@ public class SlaveCommunicator implements Runnable {
 	@Override
 	public void run() {
 		try {
-			socket = new Socket("127.0.0.1", 8000);
-			System.out.println("Client connected to server");
-			communicate();
-
+			socket = new Socket("192.168.1.3", 8000);
+			System.out.println("Client inside communicate");
+			ObjectInputStream objectInput = new ObjectInputStream(
+					socket.getInputStream());
+			Packet packet = (Packet) objectInput.readObject();
+			
 		} catch (UnknownHostException e1) {
 			e1.printStackTrace();
 		} catch (IOException e1) {
 			e1.printStackTrace();
-		}
-	}
-
-	public void communicate() {
-		try {
-			System.out.println("Client inside communicate");
-			ObjectInputStream objectInput = new ObjectInputStream(
-					socket.getInputStream());
-			Packet packet;
-			packet = (Packet) objectInput.readObject();
-			System.out.println("Client received" + packet);
-		} catch (IOException e) {
-			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-
 	}
 }
